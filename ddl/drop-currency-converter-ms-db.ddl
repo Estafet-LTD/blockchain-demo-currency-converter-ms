0caaff1d6a701ea4e037be90373f688faa9f46b0
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-01-10 15:06:23 +0000

    Applied changes to fix exchange rate

--- ddl/drop-currency-converter-ms-db.ddl
+++ ddl/drop-currency-converter-ms-db.ddl
@@ -1,3 +1,2 @@
 drop table if exists EXCHANGE_RATE cascade;
 drop table if exists MESSAGE_EVENT cascade;
-drop sequence EXCHANGE_RATE_ID_SEQ;
--- src/main/java/com/estafet/blockchain/demo/currency/converter/ms/model/ExchangeRate.java
+++ src/main/java/com/estafet/blockchain/demo/currency/converter/ms/model/ExchangeRate.java
@@ -2,10 +2,7 @@ package com.estafet.blockchain.demo.currency.converter.ms.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -13,23 +10,14 @@ import javax.persistence.Table;
 public class ExchangeRate {
 
 	@Id
-	@SequenceGenerator(name = "EXCHANGE_RATE_ID_SEQ", sequenceName = "EXCHANGE_RATE_ID_SEQ", allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "EXCHANGE_RATE_ID_SEQ")
-	@Column(name = "EXCHANGE_RATE_ID")
-	private Integer id;
-	
-	@Column(name = "FROM_CURRENCY", nullable = false)
-	private String fromCurrency;
-	
-	@Column(name = "TO_CURRENCY", nullable = false)
-	private String toCurrency;
+	@Column(name = "CURRENCY", nullable = false)
+	private String currency;
 	
 	@Column(name = "RATE", nullable = false)
 	private Double rate;
 	
-	public double convert(double fromAmount) {
-		return fromAmount * rate;
+	public double convert(double amount) {
+		return amount * rate;
 	}
-
 	
 }
