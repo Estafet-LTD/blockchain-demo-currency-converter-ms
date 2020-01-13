856b882fbfa3d76a4608140e9d98cbca50d3356e
mike2906 <michael.ruse@estafet.com>
2020-01-13 15:31:16 +0200

    First Commit - Adding JMS, DAO, Service. Updating Contoller, Exchange Rate, Pom

--- src/main/java/com/estafet/blockchain/demo/currency/converter/ms/controller/CurrencyConverterController.java
+++ src/main/java/com/estafet/blockchain/demo/currency/converter/ms/controller/CurrencyConverterController.java
@@ -1,10 +1,26 @@
 package com.estafet.blockchain.demo.currency.converter.ms.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.estafet.blockchain.demo.currency.converter.ms.model.API;
+import com.estafet.blockchain.demo.currency.converter.ms.model.ExchangeRate;
+import com.estafet.blockchain.demo.currency.converter.ms.service.CurrencyConverterService;
+import com.estafet.blockchain.demo.messages.lib.bank.BankPaymentCurrencyConverterMessage;
+import com.estafet.blockchain.demo.messages.lib.bank.BankPaymentMessage;
+
 
 @RestController
 public class CurrencyConverterController {
@@ -12,9 +28,44 @@ public class CurrencyConverterController {
 	@Value("${app.version}")
 	private String appVersion;
 	
+	@Autowired
+	private CurrencyConverterService currencyConverterService;
+	
 	@GetMapping("/api")
 	public API getAPI() {
 		return new API(appVersion);
 	}
-	
+		
+	@GetMapping(value = "/exchange-rate/{currency}")
+	public ExchangeRate getRate(@PathVariable String currency) {
+		return currencyConverterService.getExchangeRate(currency);
+	}
+		
+	@GetMapping(value = "/exchange-rates")
+	public List<ExchangeRate> getRates() {
+		return currencyConverterService.getExchangeRates();
+	}
+
+	@PostMapping(value = "/exchange-rate")
+	public ResponseEntity<ExchangeRate> addRate(@RequestBody ExchangeRate exchangeRate) {
+		return new ResponseEntity<ExchangeRate>(currencyConverterService.newExchangeRate(exchangeRate), HttpStatus.OK);
+	}
+
+	@PutMapping("exchange-rate/{currency}")
+	public ResponseEntity<ExchangeRate> updateRate(@PathVariable String currency,@RequestBody ExchangeRate exchangeRate) {
+		return new ResponseEntity<ExchangeRate>(currencyConverterService.updateExchangeRate(exchangeRate), HttpStatus.OK);
+	}
+/*
+	@DeleteMapping("/rates")
+	public ResponseEntity<String> deleteAll() {
+		currencyConverterService.deleteAll();
+		return new ResponseEntity<String>("Exchange Rates Deleted", HttpStatus.OK);
+	}
+	*/
+	@PostMapping("/testCurrencyConverter ")
+	public ResponseEntity<BankPaymentMessage> calculateSprints(@RequestBody BankPaymentCurrencyConverterMessage bankToCurrencyConvMessage) {
+		return new ResponseEntity<BankPaymentMessage>(currencyConverterService.convert(bankToCurrencyConvMessage), HttpStatus.OK);
+	}
+		
+		
 }
--- src/main/java/com/estafet/blockchain/demo/currency/converter/ms/model/ExchangeRate.java
+++ src/main/java/com/estafet/blockchain/demo/currency/converter/ms/model/ExchangeRate.java
@@ -1,11 +1,11 @@
 package com.estafet.blockchain.demo.currency.converter.ms.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -13,23 +13,73 @@ import javax.persistence.Table;
 public class ExchangeRate {
 
 	@Id
-	@SequenceGenerator(name = "EXCHANGE_RATE_ID_SEQ", sequenceName = "EXCHANGE_RATE_ID_SEQ", allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "EXCHANGE_RATE_ID_SEQ")
-	@Column(name = "EXCHANGE_RATE_ID")
-	private Integer id;
-	
-	@Column(name = "FROM_CURRENCY", nullable = false)
-	private String fromCurrency;
-	
-	@Column(name = "TO_CURRENCY", nullable = false)
-	private String toCurrency;
+	@Column(name = "CURRENCY", nullable = false)
+	private String currency;
 	
 	@Column(name = "RATE", nullable = false)
 	private Double rate;
 	
-	public double convert(double fromAmount) {
-		return fromAmount * rate;
+	public double convert(double amount) {
+		return amount * rate;
 	}
+	
+
 
 	
+	public ExchangeRate() {
+	}
+
+	public ExchangeRate(String currency, double rate) {
+		this.currency = currency;
+		this.rate = rate;
+	}
+
+
+
+	public ExchangeRate addRate() {
+		return new ExchangeRate(currency, rate);
+	}
+
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public double getRate() {
+		return rate;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	public void setRate(Double rate) {
+		this.rate = rate;
+	}
+
+	public List<ExchangeRate> toList() {
+		return toList(new ArrayList<ExchangeRate>());
+	}
+
+	private List<ExchangeRate> toList( List<ExchangeRate> exchangeRates) {
+			return exchangeRates;
+		
+	}
+/*
+	public static ExchangeRate fromJSON(String message) {
+		try {
+			return new ObjectMapper().readValue(message, ExchangeRate.class);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String toJSON() {
+		try {
+			return new ObjectMapper().writeValueAsString(this);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+*/
 }
