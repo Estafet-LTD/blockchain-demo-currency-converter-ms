6157901f0accfd31e010e5a975a6a7cf72dd562b
Miroslava Stancheva <miroslava.stancheva@estafet.com>
2020-04-27 16:01:42 +0300

    https://trello.com/c/6dNUgJrt

--- src/integration-test/java/com/estafet/blockchain/demo/currency/converter/ms/container/tests/ITCurrencyConverterTest.java
+++ src/integration-test/java/com/estafet/blockchain/demo/currency/converter/ms/container/tests/ITCurrencyConverterTest.java
@@ -1,34 +1,32 @@
 package com.estafet.blockchain.demo.currency.converter.ms.container.tests;
 
-import static io.restassured.RestAssured.delete;
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-
-import java.net.HttpURLConnection;
-
 import com.estafet.blockchain.demo.currency.converter.ms.model.ExchangeRate;
 import com.estafet.blockchain.demo.currency.converter.ms.repository.ExchangeRateRepository;
+import com.estafet.blockchain.demo.messages.lib.bank.BankPaymentBlockChainMessage;
+import com.estafet.openshift.boost.commons.lib.properties.PropertyUtils;
+import com.estafet.openshift.boost.couchbase.lib.annotation.BucketSetup;
+import com.estafet.openshift.boost.couchbase.lib.spring.CouchbaseTestExecutionListener;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import com.estafet.blockchain.demo.messages.lib.bank.BankPaymentBlockChainMessage;
-import com.estafet.openshift.boost.commons.lib.properties.PropertyUtils;
+import java.net.HttpURLConnection;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
-		properties = "classpath:integration-test.properties")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, CouchbaseTestExecutionListener.class })
 public class ITCurrencyConverterTest {
 
 	BankPaymentConsumer topic = new BankPaymentConsumer();
@@ -51,6 +49,7 @@ public class ITCurrencyConverterTest {
 	}
 
 	@Test
+	@BucketSetup("ITCurrencyConverterTest.json")
 	public void testGetExchangeRate() {
 		get("exchange-rate/GBP").then()
 			.statusCode(HttpURLConnection.HTTP_OK)
@@ -59,6 +58,7 @@ public class ITCurrencyConverterTest {
 	}
 
 	@Test
+	@BucketSetup("ITCurrencyConverterTest.json")
 	public void testGetExchangeRates() {
 		get("exchange-rates").then()
 		.statusCode(HttpURLConnection.HTTP_OK)
@@ -67,6 +67,7 @@ public class ITCurrencyConverterTest {
 	}
 
 	@Test
+	@BucketSetup("ITCurrencyConverterTest.json")
 	public void deleteExchangeRates() {
 		delete("exchange-rates").then()
 		.statusCode(HttpURLConnection.HTTP_OK)
@@ -74,6 +75,7 @@ public class ITCurrencyConverterTest {
 	}
 	
 	@Test
+	@BucketSetup("ITCurrencyConverterTest.json")
 	public void testUpdateExchangeRate() {
 		given().contentType(ContentType.JSON)
 			.body("{\"currency\": \"GBP\", \"rate\": 41.645 }")
@@ -86,6 +88,7 @@ public class ITCurrencyConverterTest {
 	}
 	
 	@Test
+	@BucketSetup("ITCurrencyConverterTest.json")
 	public void testCreateExchangeRate() {
 		given().contentType(ContentType.JSON)
 			.body("{\"currency\": \"LEV\", \"rate\": 2.456 }")
@@ -98,6 +101,7 @@ public class ITCurrencyConverterTest {
 	}
 	
 	@Test
+	@BucketSetup("ITCurrencyConverterTest.json")
 	public void testCurrencyConverter() {
 		given().contentType(ContentType.JSON)
 			.body("{\"currencyAmount\": 55.6, \"currency\": \"GBP\", \"walletAddress\": \"123456\", \"signature\": \"314249\", \"transactionId\": \"987654321\" }")
@@ -112,6 +116,7 @@ public class ITCurrencyConverterTest {
 	}
 
 	@Test
+	@BucketSetup("ITCurrencyConverterTest.json")
 	public void testConsumeNewWalletCurrencyConverterMessage() {
 		CurrenyConverterTopicProducer.send("{\"currencyAmount\": 305678, \"currency\": \"USD\", \"walletAddress\": \"345678\", \"signature\": \"32423432\", \"transactionId\": \"2481632\" }");
 		BankPaymentBlockChainMessage message = topic.consume();
